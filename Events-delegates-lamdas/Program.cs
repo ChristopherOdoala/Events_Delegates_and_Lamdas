764dde0c73f2715f8378f956672e85b3aa8641d2
Christopher Odoala <odoala21@gmail.com>
2019-10-07 10:43:38 +0100

    Updated Initial Commit

---
+++ Events-delegates-lamdas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Events_delegates_lamdas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
